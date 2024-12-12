b1a6275fbbcbaadfa4b735ad7f5f127c251ca850
sagarpatil2232004 <131273524+sagarpatil2232004@users.noreply.github.com>
2024-12-12 21:06:37 +0530

    Update Login.js

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
         }
 
         try {
-            const response = await fetch("http://localhost:4000/api/auth/login", {
+            const response = await fetch("https://mern-inotebook-e6ha.onrender.com/api/auth/login", {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
